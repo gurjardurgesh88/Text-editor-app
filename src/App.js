a3f4c42f64243958ebff6a1b90cfd5d634f666a2
gurjardurgesh88 <pateldurgesh9399@gmail.com>
2025-06-14 21:46:02 +0530

    add vercel config file for deploy

--- src/App.js
+++ src/App.js
@@ -7,76 +7,63 @@ import About from "./components/About";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState("light"); //wheather dark mode is enable or not
+  const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
+
   const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type,
     });
-    setTimeout(() => {
-      setAlert(null);
-    }, 1500);
+    setTimeout(() => setAlert(null), 1500);
   };
+
   const handleThemeChange = (color) => {
     setMode("light");
     document.body.style.backgroundColor = color;
-    showAlert("light theme applied!", "success");
+    showAlert("Light theme applied!", "success");
   };
 
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
-      showAlert("Dark mode has been enable", "success");
-      document.title = "Textutiles - Dark Mode ";
-     
+      showAlert("Dark mode has been enabled", "success");
+      document.title = "Textutiles - Dark Mode";
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
-      showAlert("Light mode has been enable", "success");
-      document.title = "Textutiles - Light Mode ";
+      showAlert("Light mode has been enabled", "success");
+      document.title = "Textutiles - Light Mode";
     }
   };
+
   return (
-    <>
-      <Router basename={process.env.PUBLIC_URL}>
-        <Navbar
-          title="Textutiles"
-          mode={mode}
-          toggleMode={toggleMode}
-          onThemeChange={handleThemeChange}
-        />
-        <Alert alert={alert} />
-        <div className="container my-3">
-          <Routes>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Navbar
+        title="Textutiles"
+        mode={mode}
+        toggleMode={toggleMode}
+        onThemeChange={handleThemeChange}
+      />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        <Routes>
           <Route
-              path="/"
-              element={
-                <TextForm
-                  showAlert={showAlert}
-                  heading="Enter the text to analyze below"
-                  mode={mode}
-                />
-              }
-            />
-            <Route
-              path="/Text-editor-app"
-              element={
-                <TextForm
-                  showAlert={showAlert}
-                  heading="Enter the text to analyze below"
-                  mode={mode}
-                />
-              }
-            />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </div>
-      </Router>
-    </>
+            path="/"
+            element={
+              <TextForm
+                showAlert={showAlert}
+                heading="Enter the text to analyze below"
+                mode={mode}
+              />
+            }
+          />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
-          
 }
 
 export default App;
